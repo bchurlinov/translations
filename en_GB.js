2071a3f2fa0fcc17317d2b7427917b199cd4b002
bchurlinov <bchurlinov@hotmail.com>
2022-07-18 13:10:59 +0200

    Update en_GB.js

--- en_GB.js
+++ en_GB.js
@@ -1,4 +1,4 @@
-export default {
+export const enGB = {
     loadingDictionaries: "Metadata is loading",
     loadingDictionariesFailed: "Loading of metadata failed",
     loadingPermissions: "Permissions are loading",
