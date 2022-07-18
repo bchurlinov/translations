0881b597fff28b35edb9fa5567a0a0004f2cb69a
Bojan <bchurlinov@hotmail.com>
2022-07-18 11:21:14 +0200

    first commit

---
+++ en-GB.js
@@ -0,0 +1,18 @@
+export default {
+    loadingDictionaries: "Metadata is loading",
+    loadingDictionariesFailed: "Loading of metadata failed",
+    loadingPermissions: "Permissions are loading",
+    loadingPermissionsFailed: "Loading of permissions failed",
+    login: "Log in",
+    loginFailed: "Login failed",
+    logout: "Log out",
+    email: "Email",
+    name: "Name",
+    username: "User name",
+    password: "Password",
+    currentPassword: "Current password",
+    newPassword: "New password",
+    repeatNewPassword: "Repeat new password",
+    changePassword: "Change password",
+    incorrectPassword: "Password is incorrect",
+};
