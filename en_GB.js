bed558e127973576f8a49cb17bcfbb968b6c2cdc
bchurlinov <bchurlinov@hotmail.com>
2022-07-19 11:32:58 +0200

    A really nice message after editing a file

--- en_GB.js
+++ en_GB.js
@@ -22,7 +22,7 @@ export default {
 'passwordUpperCaseLetterError':'',
 'passwordLowerCaseLetterError':'',
 'passwordDigitError':'',
-'currentPasswordIsRequiredError':'test',
+'currentPasswordIsRequiredError':'tests',
 'newPasswordIsRequiredError':'',
 'passwordConfirmationIsRequiredError':'',
 'applyNewPassword':'test',
