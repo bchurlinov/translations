d27213f821381b0fefb11efe2e5d1e454c14c77d
bchurlinov <bchurlinov@hotmail.com>
2022-07-19 08:57:50 +0200

    Update en_GB.js

--- en_GB.js
+++ en_GB.js
@@ -1,1 +1,18 @@
-my updated file contents
+export default {
+	'loadingDictionaries': 'Metadata is loading',
+	'loadingDictionariesFailed': 'Loading of metadata failed',
+	'loadingPermissions': 'Permissions are loading',
+	'loadingPermissionsFailed': 'Loading of permissions failed',
+	'login': 'Log in',
+	'loginFailed': 'Login failed',
+	'logout': 'Log out',
+	'email': 'Email',
+	'name': 'Name',
+	'username': 'User name',
+	'password': 'Password',
+	'currentPassword': 'Current password',
+	'newPassword': 'New password',
+	'repeatNewPassword': 'Repeat new password',
+	'changePassword': 'Change password',
+	'incorrectPassword': 'Password is incorrect',
+}
