2bbc1bf6597591dcde84a22df2b2621d1b8b691f
bchurlinov <bchurlinov@hotmail.com>
2022-07-19 11:48:48 +0200

    A really nice message after editing a file

--- de_DE.js
+++ de_DE.js
@@ -35,7 +35,7 @@ export default {
 'home':'',
 'dateRange':'',
 'organizationStructure':'',
-'cockpit.myJobs':'',
+'cockpit.myJobs':'my jobs',
 'proAnalytics':'',
 'proAnalytics.title':'',
 'proAnalytics.dashboard':'',
