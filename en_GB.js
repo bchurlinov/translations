4ddc27051af42356e9f818677762a0a961140865
bchurlinov <bchurlinov@hotmail.com>
2022-07-19 11:48:54 +0200

    A really nice message after editing a file

--- en_GB.js
+++ en_GB.js
@@ -35,7 +35,7 @@ export default {
 'home':'',
 'dateRange':'date range',
 'organizationStructure':'',
-'cockpit.myJobs':'Cockpits',
+'cockpit.myJobs':'Cockpit',
 'proAnalytics':'',
 'proAnalytics.title':'',
 'proAnalytics.dashboard':'dashboard',
