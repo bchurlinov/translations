046a0e9c1feb065af1fcd50a1f28b9a526ccb2d6
bchurlinov <bchurlinov@hotmail.com>
2022-07-20 10:59:56 +0200

    Update de_CH.js

--- de_CH.js
+++ de_CH.js
@@ -1,5 +1,5 @@
 export default {
-	"checkLog": (log) => `Technische Details finden Sie im Log ${log}`,
+	"checkLog": (log) => `Technische Details finden Sie im Log ${log} ÜÜÜ`,
 	"loadingDictionaries": "Metadaten werden geladen.",
 	"loadingDictionariesFailed": "Laden der Metadaten ist fehlgeschlagen.",
 	"loadingPermissions": "Berechtigungen werden geladen.",
