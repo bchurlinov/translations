3d747c67a26ab98bf0d56f7367698cbf386a8ed9
bchurlinov <bchurlinov@hotmail.com>
2022-07-20 10:15:29 +0200

    A really nice message after editing a file

--- en_GB.js
+++ en_GB.js
@@ -26,7 +26,7 @@ export default {
 'currentPasswordIsRequiredError':'Testsss',
 'newPasswordIsRequiredError':'',
 'passwordConfirmationIsRequiredError':'',
-'applyNewPassword':'test ones',
+'applyNewPassword':'test one',
 'passwordChangeRequestFailed':'',
 'passwordChangeSuccess':'',
 'register':'',
